f4e86d51e5f7b4b6d064ec5cda55f5530aa8cd11
alexanderciuffreda <ac053@hdm-stuttgart.de>
2021-05-11 02:53:23 +0200

    added bootstrap query all component with cards

--- web-app/client/src/components/QueryAll.vue
+++ web-app/client/src/components/QueryAll.vue
@@ -1,11 +1,46 @@
 <template>
   <div class="posts">
     <h1>CQuery All: All Key-Value Pairs in World State</h1>
-    <button v-on:click="queryAll()">Query All Assets</button>
+    <b-button v-on:click="queryAll()">Query All Assets</b-button>
+<p></p>
+    <b-container fluid>
+      <b-row>
+      <div v-bind:key="carEntry.Key" v-for="carEntry in response" class="col">
 
-    <div v-bind:key="carEntry.Key" v-for="carEntry in response">
-      <p>{{ carEntry.Key }} | {{ carEntry.Record }}</p>
-    </div>
+        <b-card
+            v-bind:title="carEntry.Record.firstName + ' '+ carEntry.Record.lastName"
+            v-bind:sub-title="'ID: ' + carEntry.Key"
+            style="min-width: 20rem; max-width: 40rem;"
+            class="mb-5"
+        >
+
+          <b-list-group flush>
+            <b-list-group-item>Geburtsdatum: {{carEntry.Record.dateOfBirth}}</b-list-group-item>
+            <b-list-group-item>Email: {{carEntry.Record.email}}</b-list-group-item>
+            <b-list-group-item>Telefon: {{carEntry.Record.phone}}</b-list-group-item>
+            <b-list-group-item>Typ: {{carEntry.Record.type}}</b-list-group-item>
+            <b-list-group-item>
+              <p>Impfungen:</p>
+
+              <div v-for="vaccination in carEntry.Record.vaccinations" :key="vaccination.vaccineName">
+                <b-list-group deck>
+                  <b-list-group-item>{{vaccination.vaccineName}}</b-list-group-item>
+                  <b-list-group-item>Datum: {{vaccination.vaccinationDate}}</b-list-group-item>
+                  <b-list-group-item>Batch ID: {{vaccination.batchID}}</b-list-group-item>
+                  <b-list-group-item>Doktor ID: {{vaccination.doctorID}}</b-list-group-item>
+                </b-list-group>
+                <p></p>
+              </div>
+            </b-list-group-item>
+
+          </b-list-group>
+        </b-card>
+
+        <!-- <p>{{ carEntry.Key }} | {{ carEntry.Record }}</p> -->
+      </div>
+      </b-row>
+    </b-container>
+
     <vue-instant-loading-spinner id = 'loader' ref="Spinner"></vue-instant-loading-spinner>
   </div>
 </template>
